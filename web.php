fa546a30c6241a2e763b8ee81c2bf90e6d4b03a8
gustavo1017 <gustavoeqr1017@gmail.com>
2018-07-09 18:56:27 -0500

    mandar un script

--- web.php
+++ web.php
@@ -15,6 +15,7 @@ Route::get('contacto',['as' => 'contactos', function(){
 /* Asi decimos que de no dar un nombre la variable se le asignaria como invitado*/
 Route::get('saludos/{nombre?}',['as' => 'saludos',function($nombre = "Invitado"){
     $html = "<h2>Contenido html</2>"; //Supuestamente ingresado por el usuario
-	return view('saludo', compact('nombre','html'));
+    $script = "<script>alert('Problema XSS - Cross Site Scripting!')</script>"; //Supuestamente ingresado por el usuario
+	return view('saludo', compact('nombre','html','script'));
    
 }])->where('nombre',"[A-Za-z]+"); //Validacion para el nombre
