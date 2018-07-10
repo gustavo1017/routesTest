bc04e72e0753d16009d8aff32c37e52efc67390f
gustavo1017 <gustavoeqr1017@gmail.com>
2018-07-09 20:53:19 -0500

    limpiando el codigo de rutas

--- web.php
+++ web.php
@@ -1,27 +1,11 @@
 <?php
 
 
-Route::get('/',['as' => 'home', function(){
-   
-	return view('home');//llamando a la vista home.php
 
-}]);
+
+Route::get('/',['as'=>'home','uses'=>'PagesController@home']);
 //Asignandole nombre a la ruta
-Route::get('contacto',['as' => 'contactos', function(){
+Route::get('contacto',['as' => 'contactos','uses'=>'PagesController@contact']);
 
-	return "Seccion de contactos";
-
-}]);
 /* Asi decimos que de no dar un nombre la variable se le asignaria como invitado*/
-Route::get('saludos/{nombre?}',['as' => 'saludos',function($nombre = "Invitado"){
-    $html = "<h2>Contenido html</2>"; //Supuestamente ingresado por el usuario
-    $script = "<script>alert('Problema XSS - Cross Site Scripting!')</script>"; //Supuestamente ingresado por el usuario
-    $consolas = [
-   	"play station 4",
-   	"Xbox One",
-   	"Wii u"
-   ];
-	return view('saludo', compact('nombre','html','script','consolas'));
-   
-
-}])->where('nombre',"[A-Za-z]+"); //Validacion para el nombre
+Route::get('saludos/{nombre?}',['as' => 'saludos','uses'=>'PagesController@saludos'])->where('nombre',"[A-Za-z]+"); //Validacion para el nombre
